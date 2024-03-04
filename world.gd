64f4088e76736eab4b32358888246197a791fa6c
Akshaj <akshaj.kanumuri@gmail.com>
2024-03-04 00:54:26 -0800

    update

--- world.gd
+++ world.gd
@@ -9,6 +9,7 @@ const Player = preload("res://player.tscn")
 const PORT = 9999
 var enet_peer = ENetMultiplayerPeer.new()
 
+
 func _unhandled_input(event):
 	if Input.is_action_pressed("quit"):
 		get_tree().quit()
@@ -36,6 +37,7 @@ func _on_join_button_pressed():
 func add_player(peer_id):
 	var player = Player.instantiate()
 	player.name = str(peer_id)
+	
 	add_child(player)
 	if player.is_multiplayer_authority():
 		player.health_changed.connect(update_health_bar)
